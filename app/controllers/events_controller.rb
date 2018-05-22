a7092acf3b11808b595fea5d92c97189d0b684ae
saynschuman <saynschuman@gmail.com>
2018-05-22 23:50:12 +0300

    events adding

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,28 @@ class EventsController < ApplicationController
 		@event = Event.find(params[:id])
 	end
 
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		@events = Event.all
+		if @event.save
+			redirect_to events_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:date)
+	end
+
+	def set_event
+		@event = Event.find(params[:id])
+	end
+
 end
